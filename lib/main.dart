7ac8c5bcdcb053cecf719e57634b9b53ffd4d520
unimegan59781 <up2061391@myport.ac.uk>
2024-05-03 10:19:37 +0100

    firebase set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Initialize Flutter binding
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   Socket sock = await Socket.connect('192.168.5.207', 80);
   runApp(MyApp(channel: sock));
 }
 
 class MyApp extends StatelessWidget {
-  final Socket channel; 
+  final Socket channel;
 
   MyApp({required this.channel});
 
@@ -29,14 +35,14 @@ class MyHomePage extends StatefulWidget {
   final Socket channel;
   final String title;
 
-  MyHomePage({Key? key, required this.title, required this.channel}) : super(key: key);
-  
+  MyHomePage({Key? key, required this.title, required this.channel})
+      : super(key: key);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   void _togglePower() {
     widget.channel.write('POWER\n');
   }
@@ -46,7 +52,6 @@ class _MyHomePageState extends State<MyHomePage> {
     widget.channel.close();
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
